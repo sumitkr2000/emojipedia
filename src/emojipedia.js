17e53e550bb2a10455ea12e8147d273b6c6943c4
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-02-10 17:16:26 +0530

    update

---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🥺",
+    name: "Pleading Face",
+    meaning:
+      "A face with furrowed eyebrows, a small frown, and large, as if begging or pleading. May also represent adoration or feeling touched by a loving gesture."
+  },
+  {
+    id: 5,
+    emoji: "🥶",
+    name: "Cold Face",
+    meaning:
+      "An icy-blue face with gritted teeth usually depicted with icicles clinging to its cheeks or jaw, as if frozen from extreme cold."
+  },
+  {
+    id: 6,
+    emoji: "🥵",
+    name: "Hot Face",
+    meaning:
+      "A reddish-orange face with furrowed eyebrows, tongue stuck out, and beads of sweat, as if overheated from high temperatures."
+  },
+  {
+    id: 7,
+    emoji: "🚀",
+    name: "Rocket",
+    meaning:
+      "A rocket being propelled into space. Sometimes used to indicate a fast increase, for instance when referring to stocks going to the moon."
+  },
+  {
+    id: 8,
+    emoji: "🍎",
+    name: "Apple",
+    meaning:
+      "A classic red apple, shown with a stem and single, green leaf on major platforms. Commonly associated with doctors and teachers."
+  },
+  {
+    id: 9,
+    emoji: "🦝",
+    name: "Raccoon",
+    meaning:
+      "A raccoon, a nocturnal mammal with a mask around its eyes. Generally depicted as a friendly, cartoon-styled, gray raccoon face."
+  }
+];
+
+export default emojipedia;
